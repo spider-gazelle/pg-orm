44d19f9ddf3bdb242df50332425dcaf765821440
Ali Naqvi <syed.alinaqvi@gmail.com>
2023-07-18 14:58:56 +0800

    feat(collection): chunks are now lazily loaded

--- src/pg-orm/collection.cr
+++ src/pg-orm/collection.cr
@@ -55,5 +55,43 @@ module PgORM
     protected def dup(builder : Query::Builder) : self
       Collection(T).new(builder)
     end
+
+    # Yields a block with the chunks in the given size.
+    #
+    # ```
+    # [1, 2, 4].in_groups_of(2, 0) { |e| p e.sum }
+    # # => 3
+    # # => 4
+    # ```
+    #
+    # By default, a new array is created and yielded for each group.
+    # * If *reuse* is given, the array can be reused
+    # * If *reuse* is an `Array`, this array will be reused
+    # * If *reuse* is truthy, the method will create a new array and reuse it.
+    #
+    # This can be used to prevent many memory allocations when each slice of
+    # interest is to be used in a read-only fashion.
+    def in_groups_of(size : Int, filled_up_with : U = nil, reuse = false, &) forall U
+      raise ArgumentError.new("Size must be positive") if size <= 0
+      offset = 0
+      loop do
+        slice = if reuse
+                  unless reuse.is_a?(Array)
+                    reuse = Array(T | U).new(size)
+                  end
+                  reuse.clear
+                  reuse
+                else
+                  Array(T | U).new(size)
+                end
+        new_builder = builder.limit(size).offset(offset)
+        Database.adapter(new_builder).select_each { |rs| slice << T.new(rs) }
+        offset += size
+        count = slice.size
+        (size - slice.size).times { slice << filled_up_with }
+        yield slice
+        break if count == 0
+      end
+    end
   end
 end
